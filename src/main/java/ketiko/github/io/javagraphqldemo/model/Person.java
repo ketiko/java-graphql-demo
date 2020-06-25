494d84dc4cbba0944b3cf590a5ea1e16ebd6c4a9
Ryan Hansen <ryan.hansen@octanner.com>
2020-06-25 07:59:05 -0600

    cleanup

--- src/main/java/ketiko/github/io/javagraphqldemo/model/Person.java
+++ src/main/java/ketiko/github/io/javagraphqldemo/model/Person.java
@@ -1,29 +1,19 @@
 package ketiko.github.io.javagraphqldemo.model;
 
 public class Person {
-  public Person() {}
+  private final String id;
+  private final String name;
 
   public Person(String id, String name) {
     this.id = id;
     this.name = name;
   }
 
-  private String id;
-  private String name;
-
   public String getId() {
     return id;
   }
 
-  public void setId(String id) {
-    this.id = id;
-  }
-
   public String getName() {
     return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 }
